3eee1c0a48229ba657e753e724f87e664fdadf36
arsalaanAli <38818326+ArsalaanAli@users.noreply.github.com>
2021-11-26 20:02:50 -0500

    country class

---
+++ Assign 4/country.py
@@ -0,0 +1,16 @@
+class Country:
+    def __init__(self, name, pop, area, continent):
+        self.name = name
+        self.pop = pop
+        self.area = area
+        self.continent = continent
+    
+    #getter methods
+    def getName(self):
+        return self.name
+    def getPopulation(self):
+        return self.pop
+    def getArea(self):
+        return self.area
+    def getContinent(self):
+        return self.continent
