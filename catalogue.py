50e060d60afb956ecbf3c5fc75224b116f748d32
arsalaanAli <38818326+ArsalaanAli@users.noreply.github.com>
2021-12-01 14:44:37 -0500

    Add files via upload

--- catalogue.py
+++ catalogue.py
@@ -1,7 +1,8 @@
 from country import Country
 class CountryCatalogue:
     def __init__(self, countryFile):
-        countryCat = set()
+        countryCat = dict()
+        self.countryCat = countryCat 
         countryFileOpened = open(countryFile, 'r')
         countryInfo = countryFileOpened.readlines()
         for country in countryInfo[1::]:
@@ -11,6 +12,42 @@ class CountryCatalogue:
             tempPop = country[2]
             tempArea = country[3].strip("\n")
             tempCountry = Country(tempName, tempPop, tempArea, tempContinent)
-            countryCat.add(tempCountry)
-    
-CountryCatalogue("data.txt")
+            countryCat[tempName] = tempCountry
+        print(countryCat)
+
+    def setPopulationOfCountry(self, countryName, value):
+        if countryName in countryCat:
+            countryCat[countryName].setPopulation(value)
+        else:
+            print(countryName, "is not in the Country Catalogue")
+    def setAreaOfCountry(self, countryName, value):
+        if countryName in countryCat:
+            countryCat[countryName].setArea(value)
+        else:
+            print(countryName, "is not in the Country Catalogue")
+    def setContinentOfCountry(self, countryName, value):
+        if countryName in countryCat:
+            countryCat[countryName].setContinent(value)
+        else:
+            print(countryName, "is not in the Country Catalogue")
+
+    def findCountry(self, country):
+        if country in countryCat.values():
+            return country
+        else:
+            return None
+
+
+
+    def addCountry(self, countryName, pop, area, cont):
+        if countryName in self.countryCat:
+            return False
+        newCountry = Country(countryName, pop, area, continent)
+        self.countryCat[countryName] = newCountry
+
+    def printCountryCatalogue(self):
+        for country in self.countryCat.keys():
+            print(self.countryCat[country])
+        
+x = CountryCatalogue("data.txt")
+x.printCountryCatalogue()
