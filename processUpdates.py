22b8c1651900f45c34f067f52ece1652d3661c12
arsalaanAli <38818326+ArsalaanAli@users.noreply.github.com>
2021-12-06 17:55:06 -0500

    processUpdates

--- processUpdates.py
+++ processUpdates.py
@@ -21,17 +21,23 @@ def isValidUpdate(record):
                 return False
             pop = update[2::]
             
-        if update[0:2]=="A=":
+        elif update[0:2]=="A=":
             if area != "":
                 return False
             if invalidDigit(update[2::]):
                 return False
             area = update[2::]
         
-        if update[0]=="C=":
+        elif update[0:2]=="C=":
             if cont != "":
                 return False
-        print(update)
+            if invalidCont(update[2::]):
+                return False
+            cont = update[2::]
+        else:
+            if update != "":
+                return False
+    return True
 
 def invalidDigit(digit):
     digit = digit.split(",")
@@ -46,7 +52,10 @@ def invalidDigit(digit):
             return True
     return False
 def invalidCont(cont):
-    
+    continents = ["Africa", "Antarctica", "Arctic", "Asia", "Europe", "North_America", "South_America"]
+    if cont in continents:
+        return False
+    return True
 def invalidName(name):
     if name[0] != name[0].upper():
         return True
@@ -54,4 +63,10 @@ def invalidName(name):
         if not char.isalpha() and char != "_":
             return True
     return False
-print(isValidUpdate(" United_States_of_America; P=328,566,312; A=83, 022, 544"))
+records = ["Canada;P=37,591,023 ", "       Germany;A=213,000;P=83,022,544       ", "       Germany ; A=213 , 000 ; P=83, 022, 544 ", "       Vietnam;P=94,469,067       ", "Mexico       ", "       Mexico;   ", "       France;;A=213,000;P = 67,067,112 ", "       France; A=213,000 ; P = 67,067,112 ; C=Europe ", "United_States_of_America;P=328,566,312 ", "United_states_of_america;P=328,566,312"]
+badRecs = ["Canada ; P=37591023  ", "Canada       P=37,591,02", "Indonesia;P 260,581,345", "Indonesia;P 260,581,345", "germany;A=213,000;P=83,022,544", "A=213,000;P=83,022,544     ", "Mexico;A=213,000;P=83,022,544;A=213,000;P=83,022,544", "France;A=213,000;A=213,067", "Vietnam;P=94,469,067;C=asia"]
+for rec in records:
+    print(isValidUpdate(rec))
+print("break")
+for rec in badRecs:
+    print(isValidUpdate(rec))
